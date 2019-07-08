a5d8f4e20005abf91fb16f41c926d0949b4dc248
Ross Perkel <rossperkel@gmail.com>
2019-07-08 17:48:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def long_planeteer_calls(array)
 end
 
 
-cheese_types = ["cheddar", "gouda", "camembert"]
+
 def find_the_cheese(array)
-  inCheeseArr = cheese_types
-  array.find {|cheese| inCheeseArr.include?(cheese)}
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|cheese| cheese_types.include?(cheese)}
 end
